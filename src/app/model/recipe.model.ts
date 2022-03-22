d1978ca649f5dc06c5f064f56ca199efb561ca3c
Novikov Ivan <ivan.novikov.1990@list.ru>
2022-03-23 01:56:02 +0300

    testing ws

--- src/app/model/recipe.model.ts
+++ src/app/model/recipe.model.ts
@@ -1,11 +1,4 @@
 export class Recipe {
-	public name: string;
-	public description: string;
-	public imagePath: string;
 
-	constructor(name: string, desc: string, imgPath: string) {
-		this.name = name;
-		this.description = desc;
-		this.imagePath = imgPath;
-	}
+	constructor(public name: string, public desc: string, public imgPath: string) {}
 }
