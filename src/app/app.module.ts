6fddcd8c9dc1865711dd50642a9d38f40f86404e
Novikov Ivan <ivan.novikov.1990@list.ru>
2022-03-31 18:37:58 +0300

    add ReactiveFormsModule, \n add logic in edit recipe component & upgrade template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ShoppingListService } from './services/shopping-list.service';
+
+import { DropdownDirective } from './directive/dropdown.directive';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,8 +15,6 @@ import { RecipesDetailComponent } from './components/recipes/recipes-detail/reci
 import { RecipesItemComponent } from './components/recipes/recipes-list/recipes-item/recipes-item.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './directive/dropdown.directive';
-import { ShoppingListService } from './services/shopping-list.service';
 import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
 
@@ -30,7 +32,12 @@ import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edi
 		RecipeStartComponent,
 		RecipeEditComponent,
 	],
-	imports: [BrowserModule, AppRoutingModule, FormsModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		ReactiveFormsModule,
+	],
 	providers: [ShoppingListService],
 	bootstrap: [AppComponent],
 })
